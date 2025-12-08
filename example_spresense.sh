e5db023330909139c1fc26a3f85c7505d27107c5
lipoyang <lipoyang1453@gmail.com>
2025-12-08 16:40:44 +0900

    add files

---
+++ example_spresense.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rm -rf ./src/example
+cp -r ./examples/spresense_avatar ./src/example
---
+++ src/Log.h
@@ -0,0 +1,1 @@
+#define LOG_PRINTF(format, ...) Serial.printf("[LOG I]" format "\n", ##__VA_ARGS__)
